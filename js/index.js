3422cdf3b532916edac468a2f205f38977ff527f
YEONJA2 <mangekyou93@gmail.com>
2026-03-17 00:58:45 +0900

    임시 커밋(5번까지 완료)

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 function toggleAudio() {
     const audio = document.getElementById('bgm');
+    const icon = document.getElementById('bgm-icon');
     if (audio.paused) {
+        icon.setAttribute('src', 'files/images/icons/pause.svg');
         audio.play();
     } else {
+        icon.setAttribute('src', 'files/images/icons/play.svg');
         audio.pause();
     }
 }
