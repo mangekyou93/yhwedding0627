7c3da9fae1d51451f4148c7ae10797b78c9a7d8e
YEONJA2 <mangekyou93@gmail.com>
2026-03-17 23:51:18 +0900

    6번 임시 커밋

--- js/index.js
+++ js/index.js
@@ -9,3 +9,15 @@ function toggleAudio() {
         audio.pause();
     }
 }
+
+function openModal() {
+    document.getElementById('modal-overlay').style.display = 'flex';
+    // 모달 열릴 때 본문 스크롤 방지 (선택사항)
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal() {
+    document.getElementById('modal-overlay').style.display = 'none';
+    // 스크롤 방지 해제
+    document.body.style.overflow = 'auto';
+}
